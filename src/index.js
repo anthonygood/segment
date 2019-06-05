bfaabfb7cfb5df57bd42f07a633d37a8e73676ce
Anthony Good <good.anthony@gmail.com>
2019-06-05 22:40:03 +0100

    update index for fetching document graph

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const jimp = require('jimp')
+const { recognize } = require('penteract')
 
 const RegionManager = require('./RegionManager/RegionManager')
 const RegionManagerConfig = require('./RegionManager/RegionManagerConfig')
@@ -10,34 +11,31 @@ const process = async filename => {
   const filepath = getPath(filename)
   const outputFile = getOutputPath(filename)
   const image = await jimp.read(filepath)
+
   const regions = await new RegionManager(image)
     .scan(3)
-    .draw(PROC_IMAGE_TO_FULL_SIZE_SCALE)
-    .save(outputFile)
+    // .draw(PROC_IMAGE_TO_FULL_SIZE_SCALE)
+    // .save(outputFile)
+    // .getText()
+    .getDocumentGraph()
 
-  open(outputFile)
+  console.log("ALL TEXT: ", regions)
 
-  await regions.draw(1, regions._image).save('region_' + outputFile, regions._image)
-  open('region_' + outputFile)
+  // open(outputFile)
+
+  // await regions.draw(1, regions._image).save('region_' + outputFile, regions._image)
+  // open('region_' + outputFile)
 
   return regions
 }
 
 const main = async () => {
-  // [
-    // 'cv',
-    // 'ft',
-    // 'gmail',
-    // 'guardian',
-    // 'wiki-excerpt',
-  // ].forEach(process)
-
   return process('CVs/CV1')
 
-  // Array.from({ length: 5 }).fill()
+  // Array.from({ length: 1 }).fill()
   //   .map((_, i) => `CVs/CV${i + 1}`)
   //   .forEach(process)
 }
 
 main()
-module.exports = main
+module.exports = process
